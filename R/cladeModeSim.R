c5ceff10174e9672e7eb139425226ba3df3b61ab
MN Puttick <mp1728@eduroam-23-16.nomadic.bris.ac.uk>
2017-04-20 16:46:25 +0100

    first commit

--- R/cladeModeSim.R
+++ R/cladeModeSim.R
@@ -18,98 +18,126 @@
 #' cladeMode()
 
 
-cladeModeSim <- function(n=1, phy, node, model=c("BM", "EB", "nestedEB", "nestedEBRate", "rateShift", "nestedOU", "OU"), beta=1, a=NULL, alpha=0.05, shiftRate=0.5, ebBeta=2) {
-	
-	beta <- log(beta)
-	shiftRate <- log(shiftRate)
-	ebBeta <- log(ebBeta)
-
-	if(model == "BM") {
-		phyMat <- vcv.phylo(phy) * exp(beta)
-		attr(phyMat, "class") <- "matrix"
-		y <- as.matrix(t(rmvnorm(n, sigma = phyMat)))
-		rownames(y) <- phy$tip.label
-		return(y)
-	}
-	
-	if(model == "nestedEB") {
-		allTimes <- BranchingTimesFossil(phy)
-  		relations_num <- monoClade(phy, node)
-  		originTimeFocal <- allTimes[which(names(allTimes) == node)]
-  		names(allTimes)[((Nnode(phy)+1):(Nnode(phy)+Ntip(phy)))] <- 1:Ntip(phy)
-  		times <- c()
-  		for(u in 1:length(allTimes)) times[which(phy$edge[,1] == names(allTimes)[u])] <- allTimes[u]
-  		branches <- match(relations_num , phy$edge[,2])
-   		if(is.null(a) == T) a <- (log(1e-4)/originTimeFocal)	/2	
-   		release.mat <- split.vcv.node(phy, node)
-  		bm.mat <- release.mat[[1]]	
-   		eb.mat <- ebSubClade(phy, a=a, node=node, branches, times, originTimeFocal)
-    	vv <- exp(beta) * (bm.mat + eb.mat)
-		y <- as.matrix(t(rmvnorm(n,  sigma = vv)))
-		rownames(y) <- phy$tip.label
-		return(y)
-		}
-  
-	if(model == "nestedEBRate") {
-		allTimes <- BranchingTimesFossil(phy)
-	  	relations_num <- monoClade(phy, node)
-	  	originTimeFocal <- allTimes[which(names(allTimes) == node)]
-	  	names(allTimes)[((Nnode(phy)+1):(Nnode(phy)+Ntip(phy)))] <- 1:Ntip(phy)
-	  	times <- c()
-	  	for(u in 1:length(allTimes)) times[which(phy$edge[,1] == names(allTimes)[u])] <- allTimes[u]
-	  	branches <- match(relations_num , phy$edge[,2])
-   		if(is.null(a) == T) a <- (log(1e-4)/originTimeFocal)	/2	
-   		release.mat <- split.vcv.node(phy, node)
-  		bm.mat <- release.mat[[1]]	 		
-   		eb.mat <- ebSubClade(phy, a=a, node, branches, times, originTimeFocal)
-   		vv <- exp(beta) * (bm.mat + (exp(ebBeta) * eb.mat))  
-	    	y <- as.matrix(t(rmvnorm(n,  sigma = vv)))
-   	 	rownames(y) <- phy$tip.label
-   	 	return(y)
-		}
-           
-	if(model == "rateShift") {
-		release.mat <- split.vcv.node(phy, node)
-		bm.mat <- release.mat[[1]]
-		shift.mat <- release.mat[[2]]
-		vv <- exp(beta) * (bm.mat + (exp(shiftRate) * shift.mat))  
-    	y <- as.matrix(t(rmvnorm(n,  sigma = vv)))
-    	rownames(y) <- phy$tip.label
-    	return(y)
-		}
- 
- 	if(model == "nestedOU") {
-		release.mat <- split.vcv.node(phy, node)
-		bm.mat <- release.mat[[1]]
-		shift.mat <- release.mat[[2]]
-		vv <- exp(beta) * (bm.mat + ouMatrix(shift.mat, exp(alpha)))
-	    y <- as.matrix(t(rmvnorm(n,  sigma = vv)))
-	    rownames(y) <- phy$tip.label
-	    return(y)
-	 	}
- 
-	if(model == "EB") {
-		node=Ntip(phy) + 1
-		allTimes <- BranchingTimesFossil(phy)
-		relations_num <- monoClade(phy, node)
-		originTimeFocal <- allTimes[which(names(allTimes) == node)]
-		names(allTimes)[((Nnode(phy)+1):(Nnode(phy)+Ntip(phy)))] <- 1:Ntip(phy)
-		times <- c()
-		for(u in 1:length(allTimes)) times[which(phy$edge[,1] == names(allTimes)[u])] <- allTimes[u]
-		if(is.null(a) == T) a <- (log(1e-4)/originTimeFocal)	/2			
-		branches <- match(relations_num, phy$edge[,2])
-	   	eb.mat <- ebSubClade(phy, a=a, node=node, branches=branches, times=times, originTime=originTimeFocal)
-    		vv <- exp(beta) * (eb.mat)  
-    		y <- as.matrix(t(rmvnorm(n,  sigma = vv)))
-    		rownames(y) <- phy$tip.label
-    		return(y)
-		}   
-	
-	if(model == "OU") {
-		vcvMat <- vcv(phy)
-	   	vv <- exp(beta) * ouMatrix(vcvMat, exp(alpha))
-		y <- as.matrix(t(rmvnorm(n,  sigma = vv)))
-		rownames(y) <- phy$tip.label
-		return(y)
-		}
+cladeMode <- function (n = 1, phy, node, model = c("BM", "EB", "nestedEB", 
+    "nestedEBRate", "rateShift", "nestedOU", "OU"), beta = 1, 
+    a = NULL, alpha = 0.05, shiftRate = 0.5, ebBeta = 2) 
+{
+    beta <- log(beta)
+    shiftRate <- log(shiftRate)
+    ebBeta <- log(ebBeta)
+    
+    if (model == "BM") {
+	        phyMat <- vcv.phylo(phy) * exp(beta)
+    	    attr(phyMat, "class") <- "matrix"
+    	    y <- as.matrix(t(rmvnorm(n, sigma = phyMat)))
+    	    rownames(y) <- phy$tip.label
+    	    return(y)
+    }
+    if (model == "nestedEB") {
+    	yy <- c()
+        for(x in 1:length(node)) {
+	        allTimes <- BranchingTimesFossil(phy)
+	        relations_num <- monoClade(phy, node[x])
+	        originTimeFocal <- allTimes[which(names(allTimes) == 
+            node[x])]
+	        names(allTimes)[((Nnode(phy) + 1):(Nnode(phy) + Ntip(phy)))] <- 1:Ntip(phy)
+	        times <- c()
+	        for (u in 1:length(allTimes)) times[which(phy$edge[, 
+            1] == names(allTimes)[u])] <- allTimes[u]
+	        branches <- match(relations_num, phy$edge[, 2])
+   		     if (is.null(a) == T) 
+            a <- (log(1e-04)/originTimeFocal)/2
+        	release.mat <- split.vcv.node(phy, node[x])
+        	bm.mat <- release.mat[[1]]
+        	eb.mat <- ebSubClade(phy, a = a, node = node[x], branches, 
+            times, originTimeFocal)
+        	vv <- exp(beta) * (bm.mat + eb.mat)
+        	y <- as.matrix(t(rmvnorm(1, sigma = vv)))
+        	yy <- cbind(yy, y)
+        	}
+        	
+        y <- yy
+        rownames(y) <- phy$tip.labels     
+        return(y)
+    }
+    if (model == "nestedEBRate") {
+    	yy <- c()
+        for(x in 1:length(node)) {
+        	allTimes <- BranchingTimesFossil(phy)
+        	relations_num <- monoClade(phy, node[x])
+        	originTimeFocal <- allTimes[which(names(allTimes) == 
+            node[x])]
+        	names(allTimes)[((Nnode(phy) + 1):(Nnode(phy) + Ntip(phy)))] <- 1:Ntip(phy)
+        	times <- c()
+        	for (u in 1:length(allTimes)) times[which(phy$edge[, 
+            1] == names(allTimes)[u])] <- allTimes[u]
+        	branches <- match(relations_num, phy$edge[, 2])
+        	if (is.null(a) == T) 
+            a <- (log(1e-04)/originTimeFocal)/2
+        	release.mat <- split.vcv.node(phy, node[x])
+        	bm.mat <- release.mat[[1]]
+        	eb.mat <- ebSubClade(phy, a = a, node[x], branches, times, 
+            originTimeFocal)
+        	vv <- exp(beta) * (bm.mat + (exp(ebBeta) * eb.mat))
+        	y <- as.matrix(t(rmvnorm(1, sigma = vv)))
+        	yy <- cbind(yy, y)
+        	}
+        	rownames(y) <- phy$tip.label
+        	return(y)
+    }
+    if (model == "rateShift") {
+    	yy <- c()
+        for(x in 1:length(node)) {
+        	release.mat <- split.vcv.node(phy, node[x])
+        	bm.mat <- release.mat[[1]]
+        	shift.mat <- release.mat[[2]]
+        	vv <- exp(beta) * (bm.mat + (exp(shiftRate) * shift.mat))
+        	y <- as.matrix(t(rmvnorm(1, sigma = vv)))
+        	yy <- cbind(yy, y)
+        	}
+        y <- yy
+        rownames(y) <- phy$tip.label
+        return(y)
+    }
+    if (model == "nestedOU") {
+    	yy <- c()
+        for(x in 1:length(node)) {
+    	    release.mat <- split.vcv.node(phy, node[x])
+        	bm.mat <- release.mat[[1]]
+        	shift.mat <- release.mat[[2]]
+        	vv <- exp(beta) * (bm.mat + ouMatrix(shift.mat, exp(alpha)))
+        	y <- as.matrix(t(rmvnorm(1, sigma = vv)))
+        	yy <- cbind(yy, y)
+        	}
+        y <- yy
+        rownames(y) <- phy$tip.label
+        return(y)
+    }
+    if (model == "EB") {
+        node = Ntip(phy) + 1
+        allTimes <- BranchingTimesFossil(phy)
+        relations_num <- monoClade(phy, node)
+        originTimeFocal <- allTimes[which(names(allTimes) == 
+            node)]
+        names(allTimes)[((Nnode(phy) + 1):(Nnode(phy) + Ntip(phy)))] <- 1:Ntip(phy)
+        times <- c()
+        for (u in 1:length(allTimes)) times[which(phy$edge[, 
+            1] == names(allTimes)[u])] <- allTimes[u]
+        if (is.null(a) == T) 
+            a <- (log(1e-04)/originTimeFocal)/2
+        branches <- match(relations_num, phy$edge[, 2])
+        eb.mat <- ebSubClade(phy, a = a, node = node, branches = branches, 
+            times = times, originTime = originTimeFocal)
+        vv <- exp(beta) * (eb.mat)
+        y <- as.matrix(t(rmvnorm(n, sigma = vv)))
+        rownames(y) <- phy$tip.label
+        return(y)
+    }
+    if (model == "OU") {
+        vcvMat <- vcv(phy)
+        vv <- exp(beta) * ouMatrix(vcvMat, exp(alpha))
+        y <- as.matrix(t(rmvnorm(n, sigma = vv)))
+        rownames(y) <- phy$tip.label
+        return(y)
+    }
 }
